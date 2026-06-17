6392069e504fd33661e58c3ecd53325d7d7c2217
Harrison Vanderbyl <33416119+harrisonvanderbyl@users.noreply.github.com>
2026-06-17 18:26:27 +1000

    Simplify installaudio.sh by removing UCM handling
    Removed UCM files copying and cleanup steps from the script.

--- installaudio.sh
+++ installaudio.sh
@@ -13,18 +13,5 @@ make
 sudo make install
 
 
-echo "Copying UCM files to /usr/share/alsa/ucm2/"
 cd ..
-# if the directory already exists, remove it first
-if [ -d "alsa-ucm-conf" ]; then
-    echo "Directory alsa-ucm-conf already exists. Removing it first."
-    rm -rf alsa-ucm-conf
-fi
-git clone https://github.com/harrisonvanderbyl/alsa-ucm-conf
-cd alsa-ucm-conf
-sudo cp -r ./ucm2/* /usr/share/alsa/ucm2/
-
-cd ..
-echo "Cleaning up"
-sudo rm -rf ./audioreach-topology
-sudo rm -rf ./alsa-ucm-conf
+echo "Just ensure have alsa-ucm-conf (1.2.15.3-1ubuntu1.2)"
